473f22ae2d8cd3413304dfab604b7fb0f628be8a
Utkarsh Sopan <utkarshsopan@Administrators-MacBook-Pro.local>
2018-09-26 13:55:08 +0530

    changed presto version to latest

--- package/scripts/presto_coordinator.py
+++ package/scripts/presto_coordinator.py
@@ -27,7 +27,7 @@ class Coordinator(Script):
     def install(self, env):
         from params import java_home
         Execute('wget --no-check-certificate {0}  -O /tmp/{1}'.format(PRESTO_RPM_URL, PRESTO_RPM_NAME))
-        Execute('export JAVA8_HOME={0} && rpm -i /tmp/{1}'.format(java_home, PRESTO_RPM_NAME))
+        Execute('export JAVA8_HOME={0} && rpm -i /tmp/{1}'.format(java_home, PRESTO_RPM_NAME),ignore_failures=True)
         self.configure(env)
 
     def stop(self, env):
